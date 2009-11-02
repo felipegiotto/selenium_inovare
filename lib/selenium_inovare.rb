1aa506004c13ff91763a169b3a1bab47a7b12df9
Felipe Giotto <felipegiotto@sol.(none)>
2009-11-02 13:45:36 -0200

    Initial Commit

---
+++ lib/selenium_inovare.rb
@@ -0,0 +1,86 @@
+#require "test/unit"
+require 'rubygems'
+require 'activesupport'
+require 'active_support/test_case'
+require "selenium/client"
+require "selenium/client/protocol"
+
+module Selenium::Client::Protocol
+
+  #  def remote_control_command_with_symbols(*args)
+  #    puts "Oi"
+  #    remote_control_command_without_symbols(*args)
+  #    puts "Oi2"
+  #  end
+  #
+  #  alias_method_chain :remote_control_command, :symbols
+
+  def remote_control_command(verb, args=[])
+    args.collect! do |arg|
+      if arg.is_a? Symbol
+        SeleniumInovare::SELECTORS[arg.to_s] || raise("Selector '#{arg}' not found in /test/selenium_inovare/selectors/*.yml")
+      else
+        arg
+      end
+    end
+    timeout(@default_timeout_in_seconds) do
+      status, response = http_post(http_request_for(verb, args))
+      raise Selenium::CommandError, response unless status == "OK"
+      response
+    end
+  end
+
+end
+
+module SeleniumInovare
+
+  DEFAULT_SELENIUM_PARAMS = {'selenium_server_port' => 4444, 'application_server_url' => 'http://localhost:3000'}
+  SELENIUM_CONFIG_FILE = RAILS_ROOT + '/test/selenium_inovare/config.yml'
+
+  unless File.exists?(SELENIUM_CONFIG_FILE)
+    File.open SELENIUM_CONFIG_FILE, 'w' do |f|
+      f.print DEFAULT_SELENIUM_PARAMS.to_yaml
+    end
+  end
+
+  PARAMS = YAML::load_file(SELENIUM_CONFIG_FILE)
+
+  SELECTORS = {}
+  Dir.glob(RAILS_ROOT + '/test/selenium_inovare/selectors/*.yml').each do |file|
+    SELECTORS.merge! YAML::load_file(file)
+  end
+
+  class TestCase < ActiveSupport::TestCase
+
+    def run(*args, &block)
+      return if method_name =~ /^default_test$/
+
+      begin
+        selenium_server_port = SeleniumInovare::PARAMS['selenium_server_port'] || raise
+        application_server_url = SeleniumInovare::PARAMS['application_server_url'] || raise
+      rescue
+        raise "File test/selenium_inovare/config.yml has an invalid structure! If you can't fix it, delete it so it can be generated again."
+      end
+
+      begin
+        @browser = Selenium::Client::Driver.new \
+          :host => 'localhost',
+          :port => selenium_server_port,
+          :browser => "*firefox",
+          :url => application_server_url,
+          :timeout_in_second => 60
+
+        browser.start_new_browser_session
+      rescue Errno::ECONNREFUSED
+        raise "It seems that Selenium Remote Control is not running! Type 'rake selenium:rc:start' before running integration tests."
+      end
+
+      super
+      browser.close_current_browser_session
+    end
+
+    attr_reader :browser
+
+  end
+
+end
---
+++ tasks/selenium_inovare_tasks.rake
@@ -0,0 +1,45 @@
+require 'selenium/rake/tasks'
+
+Selenium::Rake::RemoteControlStartTask.new do |rc|
+  require File.dirname(__FILE__) + '/../lib/selenium_inovare'
+
+  begin
+    selenium_server_port = SeleniumInovare::PARAMS['selenium_server_port'] || raise
+  rescue
+    raise "File test/selenium_inovare/config.yml has an invalid structure! If you can't fix it, delete it so it can be generated again."
+  end
+
+  rc.port = selenium_server_port
+  rc.timeout_in_seconds = 3 * 60
+  rc.background = true
+  rc.wait_until_up_and_running = true
+  rc.jar_file = "#{ RAILS_ROOT }/vendor/plugins/selenium_inovare/selenium-server-1.0.1/selenium-server.jar"
+  rc.additional_args << "-singleWindow"
+end
+
+Selenium::Rake::RemoteControlStopTask.new do |rc|
+  require File.dirname(__FILE__) + '/../lib/selenium_inovare'
+
+  begin
+    selenium_server_port = SeleniumInovare::PARAMS['selenium_server_port'] || raise
+  rescue
+    raise "File test/selenium_inovare/config.yml has an invalid structure! If you can't fix it, delete it so it can be generated again."
+  end
+
+  rc.host = 'localhost'
+  rc.port = selenium_server_port
+  rc.timeout_in_seconds = 3 * 60
+end
+
+namespace :test do
+
+  desc 'Run the integration tests in test/selenium_inovare'
+  task :selenium_inovare do
+    RAILS_ENV = 'test'
+    require File.join(File.dirname(__FILE__), '../init')
+    Dir.glob(RAILS_ROOT + '/test/selenium_inovare/*.rb').each do |file|
+      require file
+    end
+  end
+
+end
